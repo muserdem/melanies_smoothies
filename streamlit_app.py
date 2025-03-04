3dd6e82aafdfe2297d0cbd8a86d06723b7d09508
muserdem <90156467+muserdem@users.noreply.github.com>
2025-03-03 18:04:15 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
-st.title("Customize Your Smoothie :cup_with_straw:")
+streamlit.title("Customize Your Smoothie :cup_with_straw:")
 st.write("Choose up to 5 ingredients!")
 
 name_on_order = st.text_input("Name on Smoothie:")
